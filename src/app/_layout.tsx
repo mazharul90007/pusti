960aa9200611381e96c218d50a2b8b1e8b4f1e10
mazharul90007 <mazharul90007@gmail.com>
2026-06-01 01:19:22 +0600

    Status Bar added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </>
   );
 }
